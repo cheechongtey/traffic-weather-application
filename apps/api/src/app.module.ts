33ab2ee01338f0edf6da282c4ed817c35e79d239
Chee Chong <cheechongtey@gmail.com>
2024-03-03 21:22:27 +0800

    chore: introduce module folder and update module related folder path in app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config';
 import * as redisStore from 'cache-manager-redis-store';
+import { APP_FILTER } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validationSchemaForEnv } from './config/environment-variables';
-import { PersistenceModule } from './persistence/persistence.module';
-import { LocationModule } from './location/location.module';
-import { HttpModule } from './http/http.module';
-import { APP_FILTER } from '@nestjs/core';
+import { PersistenceModule } from '@/module/persistence/persistence.module';
+import { LocationModule } from '@/module/location/location.module';
+import { HttpModule } from '@/module/http/http.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 @Module({

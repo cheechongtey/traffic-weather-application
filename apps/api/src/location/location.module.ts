49c6cec5d49e2d8210bf36d8a562145242a36698
Chee Chong <cheechongtey@gmail.com>
2024-03-03 17:44:31 +0800

    chore: add http module for location module

--- apps/api/src/location/location.module.ts
+++ apps/api/src/location/location.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [LocationController],
   providers: [LocationService],
 })

9a7801b037a5c2b3e7004f1310c539dd14d63785
Chee Chong <cheechongtey@gmail.com>
2024-03-03 21:10:14 +0800

    feat: added caching for location api to store api response

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validationSchemaForEnv } from './config/environment-variables';
@@ -16,6 +19,12 @@ import { AllExceptionsFilter } from './filters/all-exceptions.filter';
       validationSchema: validationSchemaForEnv,
     }),
     PersistenceModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
     HttpModule,
     LocationModule,
   ],
--- apps/api/src/location/location.controller.ts
+++ apps/api/src/location/location.controller.ts
@@ -2,6 +2,7 @@ import {
   Controller,
   Get,
   HttpStatus,
+  Inject,
   Query,
   Res,
   UsePipes,
@@ -9,12 +10,16 @@ import {
 } from '@nestjs/common';
 import { LocationService } from './location.service';
 import { Response } from 'express';
-
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 import GetDateTimeQuery from './dto/GetLocationDateTime';
 
 @Controller('location')
 export class LocationController {
-  constructor(private service: LocationService) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private service: LocationService,
+  ) {}
 
   @Get('/')
   @UsePipes(new ValidationPipe())
@@ -23,10 +28,18 @@ export class LocationController {
     @Res() res: Response,
   ) {
     const { dateTime } = query;
+    const cachedResponse = await this.cacheManager.get(`location-${dateTime}`);
+
+    if (cachedResponse) {
+      console.log('Found cache');
+      return res.status(HttpStatus.OK).json(cachedResponse);
+    }
 
     const trafficLocationData = await this.service.getTrafficLocation(dateTime);
     // const hydatedData =
     //   this.service.hydrateTrafficCamLocation(trafficLocationData);
+
+    await this.cacheManager.set(`location-${dateTime}`, trafficLocationData);
 
     return res.status(HttpStatus.OK).json(trafficLocationData);
   }

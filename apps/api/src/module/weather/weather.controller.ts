3e927196ec1f4bcc1500efdcef5e88bf4d68df3d
Chee Chong <cheechongtey@gmail.com>
2024-03-05 01:29:30 +0800

    feat: add search history upon search for weather forecast

--- apps/api/src/module/weather/weather.controller.ts
+++ apps/api/src/module/weather/weather.controller.ts
@@ -8,12 +8,18 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
 import { WeatherService } from './weather.service';
 import GetWeatherQuery from './dto/GetWeatherQuery';
+import { AddSearchHistoryEvent } from '../search-history/events/add-search-history.event';
 
 @Controller('weather')
 export class WeatherController {
-  constructor(private service: WeatherService) {}
+  constructor(
+    private service: WeatherService,
+    private eventEmitter: EventEmitter2,
+  ) {}
 
   @Get('/')
   @UsePipes(new ValidationPipe())
@@ -22,6 +28,13 @@ export class WeatherController {
     @Res() res: Response,
   ) {
     const { dateTime, latitude, longitude } = query;
+    const event = new AddSearchHistoryEvent();
+    event.datetime = dateTime;
+    event.latitude = latitude;
+    event.longitude = longitude;
+    console.log(event);
+    this.eventEmitter.emit('search-history.store', event);
+
     const resp = await this.service.getWeatherForecast(dateTime, {
       latitude,
       longitude,
--- apps/api/src/module/weather/weather.module.ts
+++ apps/api/src/module/weather/weather.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { WeatherService } from './weather.service';
 import { WeatherController } from './weather.controller';
 import { HttpModule } from '../http/http.module';
+import { AddSearchHistoryListener } from '../search-history/listener/add-search-history.listener';
+import { SearchHistoryService } from '../search-history/search-history.service';
+import { PrismaService } from '../persistence/prisma/prisma.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [WeatherController],
-  providers: [WeatherService],
+  providers: [
+    WeatherService,
+    SearchHistoryService,
+    PrismaService,
+    AddSearchHistoryListener,
+  ],
 })
 export class WeatherModule {}

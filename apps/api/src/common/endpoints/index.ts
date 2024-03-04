38ec474d3add0cf22a24f16c3ba761d09d60789e
Chee Chong <cheechongtey@gmail.com>
2024-03-05 00:21:32 +0800

    feat: add weather forecast api

--- apps/api/src/common/endpoints/index.ts
+++ apps/api/src/common/endpoints/index.ts
@@ -1,6 +1,7 @@
 const endpoints = {
   geopifyReverseGeo: `https://api.geoapify.com/v1/batch/geocode/reverse?apiKey=${process.env.GEOPIFY_API_TOKEN}`,
   trafficImage: `https://api.data.gov.sg/v1/transport/traffic-images`,
+  weatherForecast: `https://api.data.gov.sg/v1/environment/2-hour-weather-forecast`,
 };
 
 export default endpoints;

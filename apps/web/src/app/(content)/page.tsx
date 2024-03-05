2e837d7e03787574b4847fd10f035fe9fb8d3c5a
Chee Chong <cheechongtey@gmail.com>
2024-03-05 23:05:48 +0800

    chore: add location section and remove pricing section

--- apps/web/src/app/(content)/page.tsx
+++ apps/web/src/app/(content)/page.tsx
@@ -1,15 +1,15 @@
 import Banner from '@/components/sections/banner';
 import Features from '@/components/sections/features';
 import Form from '@/components/sections/form';
-import Pricing from '@/components/sections/pricing';
+import Location from '@/components/sections/location';
 
 export default async function Home() {
   return (
     <>
       <Banner />
       <Form />
+      <Location />
       <Features />
-      <Pricing />
     </>
   );
 }

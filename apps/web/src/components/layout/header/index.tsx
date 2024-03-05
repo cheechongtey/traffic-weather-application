1012f47d22d2a92ce76e588a85dd7e54371075e5
Chee Chong <cheechongtey@gmail.com>
2024-03-05 20:56:24 +0800

    feat: add ui components

---
+++ apps/web/src/components/layout/header/index.tsx
@@ -0,0 +1,17 @@
+// import { validateRequest } from '~/server/auth';
+import Navbar from './navbar';
+
+export default async function Header() {
+  // const { session } = await validateRequest();
+  const headerText = {
+    forecast: 'Weather Forecast',
+    report: 'Report',
+  };
+  return (
+    <header className='h-20 w-full'>
+      <div className='container h-full'>
+        <Navbar headerText={headerText} />
+      </div>
+    </header>
+  );
+}

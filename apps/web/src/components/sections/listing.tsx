805b11df3cd9be5a4ee286e4403ca1647e6dbf0d
Chee Chong <cheechongtey@gmail.com>
2024-03-06 07:23:18 +0800

    chore: import location folder path

--- apps/web/src/components/sections/listing.tsx
+++ apps/web/src/components/sections/listing.tsx
@@ -1,28 +1,19 @@
 'use client';
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import FormSection from './form';
 import Location from './location';
-import { TrafficCameraData } from '@/common/type/location';
-import { endpoints } from '@/lib/endpoints';
+import LocationLoader from './location/loader';
 
 const Listing = () => {
-  const [locationData, setLocationData] = useState<TrafficCameraData[]>([]);
   const [isFetching, setIsFetching] = useState<boolean>(false);
-
-  const onFetchLocation = async (dateTime: string) => {
-    setIsFetching(true);
-    const searchParams = new URLSearchParams({ dateTime });
-    const resp = await fetch(`${endpoints.traffic}?${searchParams}`);
-    setIsFetching(false);
-    const data: TrafficCameraData[] = await resp.json();
-
-    setLocationData(data);
-  };
+  const [dateTime, setDateTime] = useState<string>('');
 
   return (
     <>
-      <FormSection onFetchLocation={onFetchLocation} isFetching={isFetching} />
-      <Location locationData={locationData} />
+      <FormSection onChangeDateTime={setDateTime} isFetching={isFetching} />
+      <Suspense fallback={<LocationLoader />}>
+        <Location dateTime={dateTime} />
+      </Suspense>
     </>
   );
 };

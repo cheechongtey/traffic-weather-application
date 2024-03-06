7934b59b6f1fc8b63ee6413641ac0ebd1f7fd01e
Chee Chong <cheechongtey@gmail.com>
2024-03-06 21:26:46 +0800

    feat: add pagination to location list

--- apps/web/src/components/sections/location/list.tsx
+++ apps/web/src/components/sections/location/list.tsx
@@ -4,14 +4,24 @@ import { TrafficCameraData } from '@/common/type/location';
 import LocationCard from '@/components/ui/location-card';
 import React, { useMemo, useState } from 'react';
 import { LocationPagination } from './pagination';
-import { calculateTotalPages, paginateData } from '@/lib/utils';
+import { calculateTotalPages, cn, paginateData } from '@/lib/utils';
 
 const LocationList = ({
   locationData,
+  selectedIndex,
+  onSelectLocation,
 }: {
   locationData: TrafficCameraData[];
+  selectedIndex: number;
+  onSelectLocation: (
+    index: number,
+    latitude: number,
+    longitude: number
+  ) => void;
 }) => {
   const [activeIndex, setActiveIndex] = useState<number>(1);
+  const [selectedLocation, setSelectedLocation] = useState<number>();
+
   const chunkData = useMemo(() => {
     return paginateData<TrafficCameraData>(locationData, activeIndex, 9);
   }, [activeIndex, locationData]);
@@ -19,15 +29,21 @@ const LocationList = ({
     return calculateTotalPages(locationData.length, 9);
   }, [locationData]);
 
+  const onSelectCard = (item: TrafficCameraData, index: number) => {
+    setSelectedLocation(index);
+    onSelectLocation(index, item.location.latitude, item.location.longitude);
+  };
+
   return (
     <div>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mb-6'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 mb-6'>
         {chunkData.map((x, index) => (
           <LocationCard
             {...x}
             name={x.location_name}
-            onClick={() => {}}
             key={index}
+            className={cn({ 'bg-secondary': selectedLocation === index })}
+            onClick={() => onSelectCard(x, index)}
           />
         ))}
       </div>

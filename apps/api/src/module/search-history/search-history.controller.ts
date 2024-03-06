72dcfdf3d3bdaa64e4d24babafca6646a9203061
Chee Chong <cheechongtey@gmail.com>
2024-03-07 00:50:28 +0800

    feat: add report endpoint and done top 10 search query per day

--- apps/api/src/module/search-history/search-history.controller.ts
+++ apps/api/src/module/search-history/search-history.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Query,
+  Res,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import GetDateTimeQuery from '../location/dto/GetLocationDateTime';
+import { Response } from 'express';
+import { SearchHistoryService } from './search-history.service';
 
 @Controller('search-history')
-export class SearchHistoryController {}
+export class SearchHistoryController {
+  constructor(private service: SearchHistoryService) {}
+
+  @Get('/')
+  @UsePipes(new ValidationPipe())
+  async getReport(@Query() query: GetDateTimeQuery, @Res() res: Response) {
+    const { dateTime } = query;
+
+    const top10Report = await this.service.getSearchHistory(dateTime);
+    return res.status(HttpStatus.OK).json({
+      top10Report,
+    });
+  }
+}
--- apps/api/src/module/search-history/search-history.service.ts
+++ apps/api/src/module/search-history/search-history.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../persistence/prisma/prisma.service';
 import { Prisma } from '@prisma/client';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class SearchHistoryService {
@@ -10,5 +11,36 @@ export class SearchHistoryService {
     return this.prisma.searchHistory.create({
       data,
     });
+  }
+
+  async getSearchHistory(date: string) {
+    // Convert the date string to a Date object
+    try {
+      const targetDate = dayjs(date).format('YYYY-MM-DD HH');
+      console.log(targetDate);
+
+      // Query the database
+      const searchHistory = await this.prisma.customSearchHistoryView.findMany({
+        where: {
+          search_date: {
+            gte: targetDate,
+            lte: targetDate,
+          },
+        },
+        take: 10,
+        orderBy: {
+          search_count: 'desc',
+        },
+      });
+      console.log(searchHistory);
+      return searchHistory;
+    } catch (error) {
+      console.log(error);
+    }
+
+    // Format the datetime to 'YYYY-MM-DD HH'
+    // searchHistory.forEach((item) => {
+    //   item.search_date = item.datetime.toISOString().slice(0, 13);
+    // });
   }
 }

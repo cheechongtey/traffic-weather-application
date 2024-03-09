a64b613c70f6480b924d58ce590351a6059b7a19
Chee Chong <cheechongtey@gmail.com>
2024-03-09 14:21:48 +0800

    feat: configure session in frontend

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,17 @@ import NextAuth from 'next-auth';
 const handler = NextAuth({
   providers: [],
   secret: process.env.NEXTAUTH_SECRET as string,
+  callbacks: {
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.sub,
+        },
+      };
+    },
+  },
 });
 
 export { handler as GET, handler as POST };

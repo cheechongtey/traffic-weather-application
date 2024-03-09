5584391d828840cbb3d95d65e4f1e0e2aa13f6b4
Chee Chong <cheechongtey@gmail.com>
2024-03-09 14:29:54 +0800

    fix: add sessionprovider

--- apps/web/src/app/(content)/page.tsx
+++ apps/web/src/app/(content)/page.tsx
@@ -1,11 +1,14 @@
+'use client';
+
 import Banner from '@/components/sections/banner';
 import Listing from '@/components/sections/listing';
+import { SessionProvider } from 'next-auth/react';
 
-export default async function Home() {
+export default function Home() {
   return (
-    <>
+    <SessionProvider>
       <Banner />
       <Listing />
-    </>
+    </SessionProvider>
   );
 }
